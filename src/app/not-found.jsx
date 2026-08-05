5871661209fb19d9bba0be80efd6b13fee138f4a
Touhid Hasan <touhidhasan1200@gmail.com>
2026-08-05 12:10:52 +0600

    added a not-found page

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+
+
+export default function NotFound() {
+  return (
+    <div className="flex min-h-screen items-center justify-center flex-col gap-4">
+      <h1 className="text-6xl font-bold">404</h1>
+      <h2>Page Not Found</h2>
+
+      
+    </div>
+  );
+}
