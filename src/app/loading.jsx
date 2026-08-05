cd48915acefbc4e9748f51625896d700c5bbef3e
Touhid Hasan <touhidhasan1200@gmail.com>
2026-08-05 12:03:07 +0600

    added a Loading Spinner

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,9 @@
 import { Spinner } from "@heroui/react";
 
 export default function Loading() {
-    return (
-        <div className="flex h-screen items-center justify-center">
-            <Spinner size="lg" />
-        </div>
-    );
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Spinner size="lg" />
+    </div>
+  );
 }
